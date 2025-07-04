26f4efa1bbd4cb9c3c798b994b1ac89d2719b8aa
v20200315 <v20200315@gmail.com>
2025-07-04 14:04:01 +0800

    feat: save image embeddings to chromadb

--- multimodal_rag_final.py
+++ multimodal_rag_final.py
@@ -5,9 +5,9 @@ from matplotlib import pyplot as plt
 from datasets import load_dataset
 
 ds = load_dataset("huggan/flowers-102-categories")
+
 # print(ds.num_rows)
-#
-# flower = ds["train"][15]["image"]
+# flower = ds["train"][78]["image"]
 # plt.imshow(flower)
 # plt.axis("off")
 # plt.show()
@@ -32,4 +32,31 @@ def save_images(dataset, dataset_folder, num_images=500):
     print(f"Saved the first 500 images to {dataset_folder}")
 
 
-save_images(ds, dataset_folder, num_images=500)
+# save_images(ds, dataset_folder, num_images=500)
+
+import chromadb
+from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
+from chromadb.utils.data_loaders import ImageLoader
+
+chroma_client = chromadb.PersistentClient(path="./data/flower.db")
+image_loader = ImageLoader()
+embedding_function = OpenCLIPEmbeddingFunction()
+flower_collection = chroma_client.get_or_create_collection(
+    "flowers_collection",
+    embedding_function=embedding_function,
+    data_loader=image_loader,
+)
+
+ids = []
+uris = []
+
+for i, filename in enumerate(sorted(os.listdir(dataset_folder))):
+    if filename.endswith(".png"):
+        file_path = os.path.join(dataset_folder, filename)
+
+        ids.append(str(i))
+        uris.append(file_path)
+
+flower_collection.add(ids=ids, uris=uris)
+print("Images added to the database.")
+print(flower_collection.count())
